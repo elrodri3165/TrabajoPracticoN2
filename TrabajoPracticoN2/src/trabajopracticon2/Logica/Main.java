28eb9797679993d577e49fc81c6bfd348f0402ee
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2021-06-19 13:32:49 -0300

    create, panel, class, Pu, Entity, MySQL connect

---
+++ TrabajoPracticoN2/src/trabajopracticon2/Logica/Main.java
@@ -0,0 +1,18 @@
+package trabajopracticon2.Logica;
+import trabajopracticon2.IGU.Panel;
+
+
+/**
+ *
+ * @author Rodrigo Gallo
+ */
+public class Main {
+    
+    public static void main(String args[]) {
+        
+       Panel princi = new Panel();
+       princi.setVisible(true);
+       princi.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ TrabajoPracticoN2/src/trabajopracticon2/Logica/Mascota.java
@@ -0,0 +1,113 @@
+package trabajopracticon2.Logica;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Rodrigo Gallo
+ */
+@Entity
+public class Mascota {
+    
+    @Id
+    private int num_cliente;
+    @Basic
+    private String nombre_perro;
+    private String raza;
+    private String Color;
+    private boolean alergico;
+    private boolean atencion_especial;
+    private String nombre_duenio;
+    private String celular_duenio;
+    private String observaciones;
+
+    public Mascota(int num_cliente, String nombre_perro, String raza, String Color, boolean alergico, boolean atencion_especial, String nombre_duenio, String celular_duenio, String observaciones) {
+        this.num_cliente = num_cliente;
+        this.nombre_perro = nombre_perro;
+        this.raza = raza;
+        this.Color = Color;
+        this.alergico = alergico;
+        this.atencion_especial = atencion_especial;
+        this.nombre_duenio = nombre_duenio;
+        this.celular_duenio = celular_duenio;
+        this.observaciones = observaciones;
+    }
+
+    public int getNum_cliente() {
+        return num_cliente;
+    }
+
+    public String getNombre_perro() {
+        return nombre_perro;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public boolean isAlergico() {
+        return alergico;
+    }
+
+    public boolean isAtencion_especial() {
+        return atencion_especial;
+    }
+
+    public String getNombre_duenio() {
+        return nombre_duenio;
+    }
+
+    public String getCelular_duenio() {
+        return celular_duenio;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setNum_cliente(int num_cliente) {
+        this.num_cliente = num_cliente;
+    }
+
+    public void setNombre_perro(String nombre_perro) {
+        this.nombre_perro = nombre_perro;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public void setAlergico(boolean alergico) {
+        this.alergico = alergico;
+    }
+
+    public void setAtencion_especial(boolean atencion_especial) {
+        this.atencion_especial = atencion_especial;
+    }
+
+    public void setNombre_duenio(String nombre_duenio) {
+        this.nombre_duenio = nombre_duenio;
+    }
+
+    public void setCelular_duenio(String celular_duenio) {
+        this.celular_duenio = celular_duenio;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    
+    
+    
+    
+}
